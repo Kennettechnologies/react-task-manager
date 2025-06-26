768638170e3e4409d561ba2dada1b71a84d6b7a6
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-26 09:51:26 +0300

    Working build and ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
-import './App.css';
-import Layout from "./components/Layout";
-import TaskManager from "./components/TaskManager";
-import Home from "./pages/Home";
-import { ThemeProvider } from "./context/ThemeContext";
 
-// Import your components here
-// import Button from './components/Button';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import TaskManager from './components/TaskManager';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <ThemeProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tasks" element={<TaskManager />} />
-            {/* Add more routes as needed */}
-          </Routes>
-        </Layout>
-      </Router>
-    </ThemeProvider>
-  );
-}
-
-export default App; 
+export default function App() {
+  return <h1>Hello, world!</h1>;
+} 
