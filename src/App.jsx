1ad41113e1cc2d7464c225d25777bbc185fd0b56
Kennettechnologies <kevinonyangowanga@gmail.com>
2025-06-26 12:39:42 +0300

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Layout from "./components/Layout";
 import TaskManager from "./components/TaskManager";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Layout>
-        <TaskManager />
-      </Layout>
+      <BrowserRouter basename="/react-task-manager">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tasks" element={<TaskManager />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 } 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTheme } from "../context/ThemeContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
@@ -7,8 +8,8 @@ const Navbar = () => {
     <nav className="bg-white dark:bg-gray-800 shadow px-4 py-3 flex justify-between items-center">
       <div className="font-bold text-xl">PLP Task Manager</div>
       <div className="flex gap-4">
-        <a href="/" className="text-gray-700 dark:text-gray-200 hover:underline">Home</a>
-        <a href="/tasks" className="text-gray-700 dark:text-gray-200 hover:underline">Tasks</a>
+        <Link to="/" className="text-gray-700 dark:text-gray-200 hover:underline">Home</Link>
+        <Link to="/tasks" className="text-gray-700 dark:text-gray-200 hover:underline">Tasks</Link>
       </div>
       <button
         className="ml-4 px-3 py-1 rounded bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100"
